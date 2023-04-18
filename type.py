d942de15e31fae52af9d6f71f8bd1dc8f701e512
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-04-18 18:12:17 +0700

    Create type.py

---
+++ type.py
@@ -0,0 +1,11 @@
+BASE_URL = 'http://27.71.26.121:5050'
+
+TIMEOUT_CHECK_TEST_CASE = 300 # 300 giay
+
+URL_CREATE_PROJECT = BASE_URL + '/projects/'
+URL_LIST_PROJECTS = BASE_URL + '/projects/'
+URL_DELETE_PROJECT = BASE_URL + '/projects/'
+URL_RETRIEVE_PROJECT = BASE_URL + '/projects/'
+URL_DUPLICATE_PROJECT = BASE_URL + '/projects/' 
+URL_EDIT_PROJECT = BASE_URL + '/projects/' 
+URL_GET_VIDEO = BASE_URL + '/projects/' 
