d3dd995063bad0af6481ba690b010959333cc16e
datdnt1027 <103656154+datdnt1027@users.noreply.github.com>
2023-05-16 17:10:51 +0700

    Update app.py

--- src/videoserver/app.py
+++ src/videoserver/app.py
@@ -100,7 +100,7 @@ def get_app(config=None):
 
 if __name__ == '__main__':
     debug = True
-    host = 'localhost'
+    host = '0.0.0.0'
     port = int(os.environ.get('PORT', '5050'))
     app = get_app()
     app.run(host=host, port=port)
